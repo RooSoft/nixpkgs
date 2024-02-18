3e6c512c59f4acf407fb0887535bc18eb78fdb81
Marc Lacoursiere <marc@roosoft.com>
2024-02-18 09:05:19 -0500

    switched to mempool-electrs

--- pkgs/applications/blockchains/esplora/default.nix
+++ pkgs/applications/blockchains/esplora/default.nix
@@ -2,37 +2,27 @@
 , stdenv
 , rustPlatform
 , fetchFromGitHub
-, fetchurl
-, rocksdb
 , Security
+, rocksdb
 }:
 
 rustPlatform.buildRustPackage rec {
-  pname = "esplora";
-  version = "20240216";
+  pname = "mempool-electrs";
+  version = "20240215";
 
   src = fetchFromGitHub {
-    owner = "Blockstream";
+    owner = "mempool";
     repo = "electrs";
-    rev = "01bd5f91f453358db40864998c5a15ce8c2c36fc";
-    hash = "sha256-FuQmedeO/8yJluju+3cx2UREU7LFuGTXPxRdYt97jNg=";
-  };
-
-  bitcoindTarball = fetchurl {
-    url = "https://bitcoincore.org/bin//bitcoin-core-25.0/bitcoin-25.0-x86_64-linux-gnu.tar.gz";
-    hash = "sha256-M5MNQyWT5J1Yqb/0wwB4gj6a9dmFlNKTWGJ4jOiiCuw=";
+    rev = "13e50239acf204df46255d4d401b4af8f0273596";
+    hash = "sha256-Ux7vlZq7oxGjCUKR4wxkfdlO7fkm8Gbefg2v6s84Ar4=";
   };
 
   cargoLock = {
     lockFile = "${src}/Cargo.lock";
     outputHashes = {
       "electrum-client-0.8.0" = "sha256-HDRdGS7CwWsPXkA1HdurwrVu4lhEx0Ay8vHi08urjZ0=";
-      "electrumd-0.1.0" = "sha256-s1/laailcwOmqjAPJnuqe7Y45Bvxwqw8EjKN54BS5gI=";
-      "jsonrpc-0.12.0" = "sha256-lSNkkQttb8LnJej4Vfe7MrjiNPOuJ5A6w5iLstl9O1k=";
     };
   };
-
-  cargoFeatures = "release";
 
   # needed for librocksdb-sys
   nativeBuildInputs = [ rustPlatform.bindgenHook ];
@@ -40,11 +30,8 @@ rustPlatform.buildRustPackage rec {
   # link rocksdb dynamically
   ROCKSDB_INCLUDE_DIR = "${rocksdb}/include";
   ROCKSDB_LIB_DIR = "${rocksdb}/lib";
-  BITCOIND_TARBALL_FILE = "${bitcoindTarball}";
 
   buildInputs = lib.optionals stdenv.isDarwin [ Security ];
-
-  # passthru.updateScript = ./update.sh;
 
   meta = with lib; {
     description = "A block chain index engine and HTTP API written in Rust based on romanz/electrs";
